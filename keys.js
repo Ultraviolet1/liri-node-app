9e42a261cd95cbfc0c8539026c773da69cb89417
Ultraviolet <tierracook543@gmail.com>
2019-02-10 23:14:55 -0600

    update js files

---
+++ keys.js
@@ -0,0 +1,11 @@
+console.log('this is loaded');
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
+
+// da3798df63794f1fa243369d2d350150
+
+// secret: 76f57baae0e54009bac410df109f302e
+ 
